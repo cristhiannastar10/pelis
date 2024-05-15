fd688bb628be1912df367e111a5ef5f1d0ac2fde
camilo177 <juanc.lopezca@campusucc.edu.co>
2024-05-15 09:04:01 -0500

    app with routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,19 @@
 import { Routes } from '@angular/router';
-import {InformationComponent} from './pages/information/information.component'
-import {ProductionsComponent} from './pages/productions/productions.component'
-import {ProfilesComponent} from './pages/profiles/profiles.component'
+import { CreateInfoComponent } from './info/create-info/create-info.component';
+import { ReadInfoComponent } from './info/read-info/read-info.component';
+import { UpdateInfoComponent } from './info/update-info/update-info.component';
+import { CreateProductionComponent } from './productions/create-production/create-production.component';
+import { ReadProductionComponent } from './productions/read-production/read-production.component';
+import { DeleteProductionComponent } from './productions/delete-production/delete-production.component';
+import { AddUserComponent } from './users/add-user/add-user.component';
 
 export const routes: Routes = [
-    { path: 'information', component: InformationComponent },
-    { path: 'productions', component: ProductionsComponent },
-    { path: 'profiles', component: ProfilesComponent },
-    { path: '', redirectTo: '/information', pathMatch: 'full' }
+    { path: 'create-info', component: CreateInfoComponent },
+    { path: 'read-info', component: ReadInfoComponent },
+    { path: 'update-info', component: UpdateInfoComponent },
+    { path: 'create-production', component: CreateProductionComponent },
+    { path: 'read-production', component: ReadProductionComponent },
+    { path: 'delete-production', component: DeleteProductionComponent },
+    { path: 'add-user', component: AddUserComponent }
+    
 ];
